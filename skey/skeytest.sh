b73ffcb2daa2cf2936431e09559172f8a8aa31d5
chaojie wang <chaojie.wang@stonybrook.edu>
2016-05-03 21:27:20 -0400

    clean up

--- skey/skeytest.sh
+++ skey/skeytest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-out=`./key -p 111 88 ka9q2`
-if test "${out}" != "BOO MALT WART FINK ONTO ANA"; then
+out=`./key -p johndoe 88 ka9q2`
+if test "${out}" != "NOLL AMRA FEE HOST BELA DEFT"; then
     echo ${out}
     exit 1
 else
